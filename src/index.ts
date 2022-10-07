50b0a6ac657ea1483b9148210c62823248234e7b
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-07 08:34:13 +0000

    feat: setup basic toolkit
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-process.stdout.write('Hello World!');
+import {Toolkit} from 'actions-toolkit';
+
+Toolkit.run(async tool => {
+	tool.log.info('Successfuly!');
+}, {event: 'push'}).catch(() => process.exit(1));

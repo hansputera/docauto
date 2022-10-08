fe155532f8e70c3317f22e98e1596911a0a15dd8
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-08 09:04:25 +0000

    chore: use ESM
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- tsup.config.cjs
+++ tsup.config.cjs
@@ -4,9 +4,7 @@ module.exports = defineConfig({
 	entry: ['./src'],
 	outDir: './dist',
 	clean: true,
-	minify: true,
 	platform: 'node',
 	target: 'node16',
-	format: 'cjs',
-	bundle: true,
+	format: 'esm',
 });
